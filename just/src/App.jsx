a57bc8cc0717b78372c0dd61df95ac98bd9ed5d2
karthik kjs <104209441+karthikgeek1@users.noreply.github.com>
2022-10-14 22:39:19 +0530

    Update App.jsx

--- just/src/App.jsx
+++ just/src/App.jsx
@@ -4,18 +4,37 @@ import axios from 'axios'
 
 const App = () => {
   const [covidData, setCovidData] = useState([])
-  // const [color, setColor] = useState(white)
+  const [filteredData, setFilteredData] = useState([])
+  const [value, setValue] = useState("")
   useEffect(()=>{
    axios.get("https://india-covid19vaccine.github.io/api/state_timeline.json").then(response=>{
     setCovidData([...response.data]);
-  }).catch(err=>err)
+    setFilteredData([...response.data])
+  }).catch(err=>console.log(err))
   },[])
+  useEffect(()=>{
+    let filterValue = covidData.filter((data)=>(
+      data.state.toLowerCase().includes(value.toLowerCase())
+    ))
+    setFilteredData([...filterValue])
+  },[value])
+  const getColor=(item)=>{
+    return item.data[107].total_fully_vaccinated>100000?'green':'red'
+  }
+  const handleclick=()=>{
+    let filterValue = covidData.filter((data)=>(
+      data.state.toLowerCase().includes(value.toLowerCase())
+    ))
+    setFilteredData([...filterValue])
+  }
   return (
-    <div>
+    <div className='myOwnHeader'>
       <center>
         <h1>INDIA COVID-19 DASHBOARD</h1>
+        <input type='text' placeholder='search' defaultValue={value} style={{marginBottom:"10px"}} onChange={(e)=>setValue(e.target.value)}/>
+        <button onClick={handleclick}>Search</button>
       <table className='table'>
-        <thead className='header'>
+        <thead className='header orange'>
           <tr>
             <th>State</th>
             <th>Total doses</th>
@@ -24,9 +43,9 @@ const App = () => {
           </tr>
         </thead>
         <tbody className='main-body'>
-          {covidData.map((item)=>{
+          {filteredData.map((item)=>{
             return (
-              <tr>
+              <tr className={getColor(item)}>
                 <td>{item.state}</td>
                 <td>{item.data[107].total_doses}</td>
                 <td>{item.data[107].total_vaccinated}</td>
